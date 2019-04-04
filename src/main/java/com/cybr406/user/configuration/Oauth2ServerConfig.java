dd4cf93615af93aaac6f6cd9c6333fd19106949c
steuhm <kered1999@live.com>
2019-04-04 14:54:30 -0500

    some stuff for final set up

--- src/main/java/com/cybr406/user/configuration/Oauth2ServerConfig.java
+++ src/main/java/com/cybr406/user/configuration/Oauth2ServerConfig.java
@@ -23,98 +23,79 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
 public class Oauth2ServerConfig {
 
 
-        @Configuration
-        @EnableResourceServer
-        protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
-            @Override
-            public void configure(ResourceServerSecurityConfigurer resources) {
-                resources.resourceId("profile").stateless(true);
-            }
-
-            @Override
-            public void configure(HttpSecurity http) throws Exception {
-                // @formatter:off
-                http
-                        .requestMatchers().antMatchers("/profiles/**")
-                        .and()
-                        .authorizeRequests()
-                        .antMatchers(HttpMethod.GET, "/profiles/**").permitAll()
-                        .antMatchers("/profiles/**", "/profiles", "/posts", "/posts/**")
-                        .access("#oauth2.isOAuth() and hasAnyRole('ROLE_BLOGGER', 'ROLE_ADMIN')");
-                // @formatter:on
-            }
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resources) {
+            resources.resourceId("profile").stateless(true);
         }
 
-        @Configuration
-        @EnableAuthorizationServer
-        protected static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                    .requestMatchers().antMatchers("/profiles/**")
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.GET, "/profiles/**").permitAll()
+                    .antMatchers(HttpMethod.POST, "/signup").permitAll()
+                    .antMatchers("/profiles/**", "/profiles", "/posts", "/posts/**")
+                    .access("#oauth2.isOAuth() and hasAnyRole('ROLE_BLOGGER', 'ROLE_ADMIN')");
+            // @formatter:on
+        }
+    }
 
-            @Autowired
-            private TokenStore tokenStore;
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        private TokenStore tokenStore;
 
 
 
-            @Autowired
-            private AuthenticationManager authenticationManager;
+        @Autowired
+        private AuthenticationManager authenticationManager;
 
-            @Autowired
-            PasswordEncoder passwordEncoder;
+        @Autowired
+        PasswordEncoder passwordEncoder;
 
 
-            @Override
-            public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 
-                // @formatter:off
-                clients.inMemory()
+            // @formatter:off
+            clients.inMemory()
 
-                        .withClient("api")
-                            .resourceIds("profile", "post")
-                            .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
-                            .authorities("ROLE_BLOGGER")
-                            .scopes("read", "write", "trust")
-                            .secret(passwordEncoder.encode(""))
-                            .and()
-                        .withClient("post")
-                            .authorities("ROLE_POST_SERVICE")
-                            .secret(passwordEncoder.encode("secret"));
-                // @formatter:on
-            }
-
-            @Bean
-            public TokenStore tokenStore() {
-                return new InMemoryTokenStore();
-            }
-
-            @Override
-            public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-                endpoints.tokenStore(tokenStore).authenticationManager(authenticationManager);
-            }
-
-            @Override
-            public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-                security
-                        .checkTokenAccess("hasRole('ROLE_POST_SERVICE')");
-            }
+                    .withClient("api")
+                    .resourceIds("profile", "post")
+                    .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+                    .authorities("ROLE_BLOGGER")
+                    .scopes("read", "write", "trust")
+                    .secret(passwordEncoder.encode(""))
+                    .and()
+                    .withClient("post")
+                    .authorities("ROLE_POST_SERVICE")
+                    .secret(passwordEncoder.encode("secret"));
+            // @formatter:on
         }
 
+        @Bean
+        public TokenStore tokenStore() {
+            return new InMemoryTokenStore();
+        }
 
-//        protected static class Stuff {
-////
-////            @Autowired
-////            private ClientDetailsService clientDetailsService;
-////
-////            @Autowired
-////            private TokenStore tokenStore;
-////
-////            @Bean
-////            public ApprovalStore approvalStore() throws Exception {
-////                TokenApprovalStore store = new TokenApprovalStore();
-////                store.setTokenStore(tokenStore);
-////                return store;
-////            }
-////
-////        }
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.tokenStore(tokenStore).authenticationManager(authenticationManager);
+        }
 
-
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+            security
+                    .checkTokenAccess("hasRole('ROLE_POST_SERVICE')");
+        }
+    }
 }
